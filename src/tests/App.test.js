d949dac98b9326343512dcf9141c034c9449ff4d
Leckar <gusleckar@gmail.com>
2022-10-13 05:50:19 -0300

    finally done

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -32,6 +32,8 @@ describe('Tests the Star Wars project', () => {
     expect(valueFilter).toBeInTheDocument();
     const buttonFilter = screen.getByTestId('button-filter');
     expect(buttonFilter).toBeInTheDocument();
+    const buttonRemoveFilter = screen.getByTestId('button-remove-filters');
+    expect(buttonRemoveFilter).toBeInTheDocument();
     // Sort elements
     const columnSort = screen.getByTestId('column-sort');
     expect(columnSort).toBeInTheDocument();
@@ -50,27 +52,48 @@ describe('Tests the Star Wars project', () => {
   });
   it('should filter correctly and list filters correctly', async () => {
     render(<App />);
-    // Filter elements
-    const nameFilter = screen.getByTestId('name-filter');
-    expect(nameFilter).toBeInTheDocument();
-    const columnFilter = screen.getByTestId('column-filter');
-    expect(columnFilter).toBeInTheDocument();
-    const comparisonFilter = screen.getByTestId('comparison-filter');
-    expect(comparisonFilter).toBeInTheDocument();
-    const valueFilter = screen.getByTestId('value-filter');
-    expect(valueFilter).toBeInTheDocument();
-    const buttonFilter = screen.getByTestId('button-filter');
-    expect(buttonFilter).toBeInTheDocument();
-    // userEvents
     await waitFor(() => {
       expect(screen.getAllByTestId(PLANET_NAME).length).toBe(UNFILTERED_NUMBER);
     });
+    const nameFilter = screen.getByTestId('name-filter');
     userEvent.type(nameFilter, 'in');
     expect(screen.getAllByTestId(PLANET_NAME).length).toBe(FILTERED_BY_NAME);
+    const columnFilter = screen.getByTestId('column-filter');
     userEvent.selectOptions(columnFilter, 'population');
+    const comparisonFilter = screen.getByTestId('comparison-filter');
     userEvent.selectOptions(comparisonFilter, 'maior que');
-    userEvent.type(valueFilter, 1000);
+    const valueFilter = screen.getByTestId('value-filter');
+    userEvent.clear(valueFilter);
+    userEvent.type(valueFilter, '0');
+    expect(valueFilter).toHaveValue(0);
+    const buttonFilter = screen.getByTestId('button-filter');
+    expect(buttonFilter).toBeInTheDocument();
     userEvent.click(buttonFilter);
-    expect(screen.getAllByTestId(PLANET_NAME).length).toBe(FILTERED_BY_NAME_AND_POP);
+    await waitFor(() => {
+      expect(screen.getAllByTestId(PLANET_NAME).length).toBe(FILTERED_BY_NAME);
+    });
+    expect(screen.getByText('population maior que 0')).toBeInTheDocument();
+    const buttonRemoveFilter = screen.getByTestId('button-remove-filters');
+    userEvent.click(buttonRemoveFilter);
+    await waitFor(() => {
+      expect(screen.getAllByTestId(PLANET_NAME).length).toBe(FILTERED_BY_NAME);
+    });
+  });
+  it('should sort the elements correctly', async () => {
+    render(<App />);
+    await waitFor(() => {
+      expect(screen.getAllByTestId(PLANET_NAME).length).toBe(UNFILTERED_NUMBER);
+    });
+    const columnSort = screen.getByTestId('column-sort');
+    userEvent.selectOptions(columnSort, 'diameter');
+    const inputDescSort = screen.getByTestId('column-sort-input-desc');
+    userEvent.click(inputDescSort);
+    const inputAscSort = screen.getByTestId('column-sort-input-asc');
+    userEvent.click(inputAscSort);
+    const buttonSort = screen.getByTestId('column-sort-button');
+    userEvent.click(buttonSort);
+    userEvent.selectOptions(columnSort, 'surface_water');
+    userEvent.click(inputDescSort);
+    userEvent.click(buttonSort);
   });
 });
